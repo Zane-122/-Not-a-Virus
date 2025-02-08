dedb7a59ace250912183bd02a7639d1331b36c46
rockit456 <zbeidas@icloud.com>
2025-02-08 13:00:12 -0500

    [#6] I cleaned code

--- src/Screens/Screen.java
+++ src/Screens/Screen.java
@@ -3,6 +3,7 @@ package Screens;
 import javax.swing.JFrame;
 
 public abstract class Screen {
+    // All things that extend screen must contain these methods
     public abstract void constructWindow(JFrame frame, int width, int height);
     public abstract void updateWindow();
 }
--- src/Window.java
+++ src/Window.java
@@ -1,16 +1,20 @@
 import java.awt.MouseInfo;
-import java.awt.Point;
 
 import javax.swing.JFrame;
 
 import Screens.Screen;
 
 public class Window {
+    // Dimensions
     int width;
     int height;
+
+    // Stuff to create window
     String name;
     JFrame frame;
     Screen screen;
+
+    // Points
     int x;
     int y;
 
@@ -23,38 +27,48 @@ public class Window {
         frame = new JFrame(name);
     }
     
+    // Add a screen to the window
     public void constructWindow() {
         screen.constructWindow(frame, width, height);
     }
 
+    // Contains everything that must run in main update loop
     public void updateWindow() {
+        // Get mouse positions
         x = (int) MouseInfo.getPointerInfo().getLocation().getX();
         y = (int) MouseInfo.getPointerInfo().getLocation().getY();
 
+        // Run things to update
         screen.updateWindow();
         moveAwayFrom(x, y, 250);
         setBorder(100, 600, 100, 1100);
     }
 
+    // Will move away from provided position
     public void moveAwayFrom(int x, int y, int maxSpeed) {
         int thisX = frame.getX();
         int thisY = frame.getY();
-    
+        
+        // Find the distance between x&y values of the two points
         int dx = thisX - x;
         int dy = thisY - y;
         
+        // Finding the distance between the two points on a 2d plane
         double distance = Math.sqrt(dx * dx + dy * dy);
-    
-        if (distance < 1) return;
-    
+        
+        if (distance < 1) return; // No 0 division
+        
+        // Make sure speed stays below max speed
         double speedFactor = Math.min(maxSpeed, maxSpeed * (5.0 / distance));
-    
+        
+        // Find how much the x and y values of the window should move by
         int moveX = (int) (dx / distance * speedFactor);
         int moveY = (int) (dy / distance * speedFactor);
-    
+        
         frame.setLocation(thisX + moveX, thisY + moveY);
     }
 
+    // Litterly just sets borders with if statements
     public void setBorder(int xBorderLeft, int yBorderBottom, int yBorderTop, int xBorderRight) {
         if (frame.getLocation().getX() < xBorderLeft) 
             {frame.setLocation(xBorderLeft + 5, frame.getY());}
@@ -64,9 +78,5 @@ public class Window {
             {frame.setLocation(xBorderRight - 5, frame.getY());}
         if (frame.getLocation().getY() < yBorderTop) 
             {frame.setLocation(frame.getX(), yBorderTop + 5);}
-    }
-
-    public Point getPosition() {
-        return new Point(frame.getX(), frame.getY());
     }
 }
