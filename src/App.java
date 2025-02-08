f5c933ea6a3641d808b3df6e27b9bec35df0c5a8
zuesdajuice <krajendran275@gmail.com>
2025-02-08 10:35:48 -0500

    [#1] Yay first commit

--- src/App.java
+++ src/App.java
@@ -9,7 +9,7 @@ public class App {
         List<Window> windows = new ArrayList<Window>();
         Screen[] screens = new Screen[] {new CountdownScreen(), new UsernameScreen()};
         Random random = new Random();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 1; i++) {
             windows.add(new Window(300, 300, "WINDOW " + i, screens[random.nextInt(screens.length)]));
             windows.get(i).constructWindow();
         }
